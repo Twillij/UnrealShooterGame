78d35112d3a06766d5493579e390fc648cafe1a1
Will <silentwill29@gmail.com>
2024-06-10 22:23:41 +1000

    Scrapped multiplayer session system
    Discovered that the session system is redundant when it comes to dedicated server models

--- Source/UnrealShooter/MultiplayerSubsystem.h
+++ Source/UnrealShooter/MultiplayerSubsystem.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "OnlineSessionSettings.h"
-#include "OnlineSubsystem.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "MultiplayerSubsystem.generated.h"
 
@@ -13,33 +11,7 @@ class UNREALSHOOTER_API UMultiplayerSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
 
-public:
-	FOnlineSessionSettings DefaultSessionSettings;
-	FOnlineSessionSearch DefaultSessionSearch;
-	TSharedPtr<FOnlineSessionSearch> LastSessionSearch;
-	
 protected:
-	UPROPERTY(BlueprintReadOnly)
-	FName OnlineSubsystemName = NAME_None;
-	
-	IOnlineSessionPtr SessionInterface = nullptr;
-	TArray<FName> CurrentSessionNames;
-
-public:
-	UFUNCTION(BlueprintCallable, Server, Reliable, WithValidation)
-	void CreateSession(const FName SessionName);
-
-	UFUNCTION(BlueprintCallable, Server, Reliable, WithValidation)
-	void DestroySession(const FName SessionName);
-
-	UFUNCTION(BlueprintCallable)
-	void FindSession(const FName SessionName);
-	
-protected:
-	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-	virtual void Deinitialize() override;
-
-	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
-	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
-	void OnFindSessionComplete(bool bWasSuccessful);
+	//virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	//virtual void Deinitialize() override;
 };
